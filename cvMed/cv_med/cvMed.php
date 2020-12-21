18cca8da23adfbe6de4bdde19c42f6d91d513b7d
medMigar <tardamed@gmail.com>
2020-12-21 22:29:51 +0100

    Ajout du CV

---
+++ cvMed/cv_med/cvMed.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>cv Med RAGIM</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../css/style.css">
+	<link rel="stylesheet" href="../../css/style_hor.css">
+</head>
+<body>
+	<div id="conteneur">
+		<?php require_once'../../parties/header.php' ?><hr>	
+		<div style="text-align: center;background-color: #f2f1f3;">	
+		<img src="../../images/alam.gif" alt="" style="height:auto;width:15%;float: left;">
+		<img src="../../images/basmala.jpg" alt="" style="height:auto;width:15%;">
+		<img src="../../images/alam.gif" alt="" style="height:auto;width:15%;float: right;">
+	</div>
+		<hr>
+		<h1>Curriculum Vitæ de RAGIM Mohamed</h1><hr>
+		<div style="text-align: center; background-color: #fff;">
+			<img src="../../images/001.jpg" alt="" style="height:auto;max-width:50%;">
+			<img src="../../images/med.jpg" alt="med" style="height:auto;max-width:100%;border-radius: 50%;border: solid 3px #d6e498;">	
+			<img src="../../images/001.jpg" alt="" style="height:auto;max-width:50%;">
+		</div><hr>
+		<article>
+			
+			<h3> Ma vie </h3>
+			<ul class="parhpe">
+				<li>Nationalité Marocaine</li>
+				<li>Né en 1962 à Zaouia Ech Cheikh</li>					
+				<li>L'enseignement primaire à Tigmmath et Zaouia Ech Cheikh </li>
+				<li>L'enseignement collégial à El Ksiba collège Moha Ou Said</li>
+				<li>Les études secondaires à Beni Mellal Lycée Hassan II </li>					
+				<li>Diplôme de l'ENS Takaddoum à Rabat</li>
+			</ul>
+			<br>
+			<h3>Ma formation</h3>
+			<ul class="parhpe">
+				<li>Ancien élève de L'ENS Takaddoum de Rabat</li>
+				<li>Autodidacte à partir des sites internets</li>
+			</ul>
+			<br>
+			<h3> Mon expérience </h3>
+			<ul class="parhpe">
+				<li>Enseignant en classe depuis: 16 - 09 - 1986 jusqu'à O1 - 08 - 2017</li>
+				<li>membre dans plusieurs associations locaux et nationaux </li>
+			</ul>
+			<br>
+			<h3>Mes compétences</h3>
+			<ul class="parhpe">
+				<li>HTML5</li>
+				<li>CSS3</li>
+				<li>JavaScript</li>
+				<li>SQL</li>
+				<li>PHP</li>
+				<li>Java EE</li>
+			</ul>
+			
+		</article>
+		<footer class="pied"><br>
+			Développé par: RAGIM Mohamed <br>
+			جميع حقوق النشر محفوظة © 2017<br>
+			<a href="/site/index.php">الرئيسية</a><br><br>
+		</footer>			
+	</div>	
+</body>
+</html>
