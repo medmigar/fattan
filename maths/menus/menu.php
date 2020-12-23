a16ca704d7f68d4b2c10a0bdf26ff4e0c78c39e5
medMigar <tardamed@gmail.com>
2020-12-23 18:08:48 +0100

    Ajout du dossier Maths

---
+++ maths/menus/menu.php
@@ -0,0 +1,38 @@
+<nav class="nav1">
+    <!-- Premier icôme 
+    ***************************
+    -->
+    <span class="iconContainer">
+        <a href="/site/index.php" class="roundIcon timeline__roundIcon">
+        <i class="fa fa-home fa-fw"></i>
+        </a>
+    </span>
+
+    <div class="timeline__steps">
+    <!-- 
+    bloc 1
+    *******************************
+    -->
+    <span class="bloc"></span>
+    
+    <a href="/site/index.php" class="timeline__step"><span class="c2">Accueil</span></a>
+    <!-- 
+    bloc 2
+    *******************************
+    -->
+    <span class="bloc"></span>
+    
+    <a href="/site/JavaScript/index.php" class="timeline__step"><span class="c2">Introduction</span></a>
+    
+    <!-- Dermier icôme 
+    ***************************
+    -->
+    <span class="bloc"></span>
+  </div>
+  <span class="iconContainer">
+    <a href="#" title="Certificat de réussite (voir un exemple)" class="roundIcon timeline__roundIcon">
+      <i class='fas fa-fist-raised' style='font-size:20px;color:#fff'></i>
+    </a>
+  </span>
+
+</nav>
